0244b5092e01ee44247aedd7961e933eb812740f
N <>
2024-10-22 08:33:33 +0200

    Test

--- gui.py
+++ gui.py
@@ -1,1 +1,8 @@
-gui
+import subprocess
+from nicegui import ui
+
+ui.button(
+    'NICHT DRUECKEN!',
+    on_click=lambda: subprocess.run("shutdown /h", shell=True, capture_output=True, text=True))
+
+ui.run()
